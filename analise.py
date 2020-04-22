3d65e1bc2ae21e97dd083a0d61205445ec9bd913
Cassio.Placido <silenozlt@hotmail.com>
2020-04-22 17:59:18 -0300

    Bucando o arquivo. Alterando a visualização para ate 1000 linhas.

--- analise.py
+++ analise.py
@@ -3,17 +3,17 @@ import pandas as pd
 import base64
 
 def main():
-    st.title('AceleraDev - Data Science')
-    #st.image('logo.png')
-    file = st.file_uploader('Choose your file', type = 'csv')
+    st.title('Analisando Dados')
+    #st.image('pandas.jpg')
+    file = st.file_uploader('Choose your file :', type = 'csv')
     if file is not None:
-        slider = st.slider('Valores', 1, 100)
+        slider = st.slider('Valores', 1, 1000)
         df = pd.read_csv(file)
         st.dataframe(df.head(slider))
-        st.markdown('Tabela')
-        st.table(df.head(slider))
-        st.write(df.columns)
-        st.table(df.groupby('species')['petal_width'].mean())
+        #st.markdown('Tabela')
+        #st.table(df.head(slider))
+        #st.write(df.columns)
+        #st.table(df.groupby('species')['petal_width'].mean())
 
 
 
