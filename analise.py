1b46f50958c086448833c9421fd7007fc9a88c3b
silenozlt <silenozlt@hotmail.com>
2020-04-22 17:26:37 -0300

    Add files via upload

---
+++ analise.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+import base64
+
+def main():
+    st.title('AceleraDev - Data Science')
+    #st.image('logo.png')
+    file = st.file_uploader('Choose your file', type = 'csv')
+    if file is not None:
+        slider = st.slider('Valores', 1, 100)
+        df = pd.read_csv(file)
+        st.dataframe(df.head(slider))
+        st.markdown('Tabela')
+        st.table(df.head(slider))
+        st.write(df.columns)
+        st.table(df.groupby('species')['petal_width'].mean())
+
+
+
+
+
+if __name__ == '__main__':
+    main()
