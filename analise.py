cfd8ea6fcc05a240ea34d59daf0b349cdc7253c1
Cassio.Placido <silenozlt@hotmail.com>
2020-04-24 19:45:18 -0300

    Mensagem de erro

--- analise.py
+++ analise.py
@@ -1,22 +1,21 @@
 import streamlit as st
 import pandas as pd
 import base64
-#import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
-
 
 
 def main():
     st.title('Analisando Dados')
     st.image('imagens/python.png')
-    file = st.file_uploader('Choose your file :', type = 'csv')
+    file = st.file_uploader('Choose your file :', type='csv')
     if file is not None:
         st.success('Carregado com sucesso !')
         st.markdown('Selecione a quantidade de linhas para visualizar: ')
         slider = st.slider('Valores', 1, 1000)
         df = pd.read_csv(file)
-        #st.dataframe(df.head(slider))
-        #st.markdown('Dados apresenteados formato tabela')
+        # st.dataframe(df.head(slider))
+        # st.markdown('Dados apresenteados formato tabela')
         st.table(df.head(slider))
         st.markdown('Conhecendo as colunas do dataset :')
         st.write(df.columns)
@@ -26,14 +25,13 @@ def main():
 
 
 
+    else:
+        st.warning('Ocorreu algum erro !')
+
     if st.button('Like'):
         st.write('Obrigado pela visita')
     else:
         st.warning('This is a warning')
-
-
-
-
 
 
 if __name__ == '__main__':
